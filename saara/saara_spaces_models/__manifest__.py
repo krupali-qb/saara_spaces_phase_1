3093ec9ab7b493612f3b75feb65d06b807b409f9
krupali-qb <krupali@quantumbot.co.in>
2025-03-27 19:01:40 +0530

    some change added.

--- saara/saara_spaces_models/__manifest__.py
+++ saara/saara_spaces_models/__manifest__.py
@@ -1,31 +1,37 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    'name': 'Customize Company Backend web icon',
-    'version': '17.0.0.1',
-    'category': 'Extra Tools',
-    'icon': '/bb_web_company_favicon/static/description/icon.png',
-    'sequence': 20,
-    'author': '',
-    'summary': 'The custom web window Favicon',
-    'description': """
-        Customize Favicon
-            """,
-    'depends': ['web'],
+    'name': 'interior project',
+    'version': '17.0.3.0.3',
+    'category': '',
+    'summary': "",
+    'author': 'quantumbot',
+    'company': 'quantumbot',
+    'maintainer': 'quantumbot',
+    'website': "https://quantumbot.in/",
+    'depends': ['web', 'base', 'mail','base_import','website'],
     'data': [
-        'views/res_company.xml',
+        'security/security_template.xml',
+        'security/ir.model.access.csv',
+        'views/interior_projects_view.xml',
+        'views/payment_method_view.xml',
+        'views/vendor_payment_method_view.xml',
+        'views/res_customer_view.xml',
+        'views/res_agency_view.xml',
+        'views/menuitem_hide.xml',
+        'views/expenses_category_view.xml',
+        'views/agency_category_view.xml',
+        'views/project_expense_view.xml',
+        'views/weblogin_template.xml',
     ],
-    'installable': True,
-    'auto_install': False,
-    "application": True,
-    'license': 'LGPL-3',
     'assets': {
         'web.assets_backend': [
-            'bb_web_company_favicon/static/src/js/favicon.js',
+            'saara_spaces_models/static/src/**/*.xml',
         ],
     },
-    'qweb': [
 
-    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'sequence': 2,
 }
